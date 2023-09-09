cacd1eb3e9f91efdade33f2dbbb3587f57bbb90b
h22j11 <115646822+h22j11@users.noreply.github.com>
2023-09-09 17:21:29 +0800

    Add files via upload
    对bag类的bug进行了更新

--- Bag.cpp
+++ Bag.cpp
@@ -9,32 +9,30 @@ extern vector<Equipment*> equipment_bag;
 
 void Bag::bag_write() {
 	int size = equipment_bag.size();
+	cout << "开始存档" << endl;
 	ofstream ofs("data/Bag/bag.dat", ios::out | ios::binary);
 	if (!ofs)
 		cout << "您的存档喝了孟婆汤，请快去开始新的游戏吧！" << endl;
 	else {
 		for (int i = 0; i < size; i++) {
-			cout << size << endl;
 			ofs.write(reinterpret_cast<char*>(equipment_bag[i]), sizeof(Equipment));
-			equipment_bag[i]->show();
 		}
 	}
 	cout << "存档成功" << endl;
+	equipment_bag.clear();
 	ofs.close();
 }
-
-
 void Bag::bag_read() {
 	ifstream ifs("data/Bag/bag.dat", ios::in | ios::binary);
 	int i = 0;
-	Equipment* l = new Equipment;
 	cout << "开始读取" << endl;
 	while (!ifs.eof()) {
+		Equipment* l = new Equipment;
 		ifs.read(reinterpret_cast<char*>(l), sizeof(Equipment));
-		if(!ifs.eof())
+		if (!ifs.eof()) {
 			equipment_bag.push_back(l);
-		cout << "读取成功" << endl;
-		equipment_bag[i]->show();
-		i++;
+			i++;
+		}
 	}
+	cout << "读取成功" << endl;
 }
